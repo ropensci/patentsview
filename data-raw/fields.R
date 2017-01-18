cec50eee1bea99780713aa172689f4bc87fb64af
Chris Baker <chriscrewbaker@gmail.com>
2017-01-17 21:49:45 -0500

    Add web scraper of patentsview fields tables

---
+++ data-raw/fields.R
@@ -0,0 +1,34 @@
+library(XML)
+library(reshape2)
+library(dplyr)
+library(devtools)
+
+c("patent","inventor", "assignee", "location",
+  "cpc_subsection", "uspc", "nber_subcat") -> endpoints
+
+sapply(endpoints, FUN = function(x) {
+  paste0('http://www.patentsview.org/api/', x, ".html") -> url
+  readHTMLTable(doc = url) -> tab
+  tab[[2]]
+}, USE.NAMES = TRUE, simplify = FALSE) -> all_tabs
+
+clean_field <- function(x) gsub("[^[:alnum:]_]", "", tolower(as.character(x)))
+
+melt(all_tabs) %>%
+  rename(field = `API Field Name`, data_type = Type, can_query = Query,
+         endpoint = L1) %>%
+  select(endpoint, field, data_type, can_query) %>%
+  mutate_each(funs(clean_field)) %>%
+  mutate(endpoint = case_when(
+    .$endpoint == "patent" ~ "patents",
+    .$endpoint == "inventor" ~ "inventors",
+    .$endpoint == "assignee" ~ "assignees",
+    .$endpoint == "location" ~ "locations",
+    .$endpoint == "cpc_subsection" ~ "cpc_subsections",
+    .$endpoint == "uspc" ~ "uspc_mainclasses",
+    .$endpoint == "nber_subcat" ~ "nber_subcategories"
+  )) -> fields
+
+write.csv(fields, "data-raw/fields.csv", row.names = FALSE)
+# tools::checkRdaFiles("R/")
+use_data(fields, internal = TRUE, overwrite = TRUE, compress = "gzip")
