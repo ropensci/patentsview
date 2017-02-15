dcb9fef3f825a466db220c03ded2ec7b5a0194b2
Chris Baker <chriscrewbaker@gmail.com>
2017-02-14 20:17:36 -0500

    Add draft version of search_pv

---
+++ R/process-resp.R
@@ -0,0 +1,44 @@
+get_request <- function(resp) {
+  structure(
+    list(
+      method = resp$req$method,
+      url = resp$req$url
+    ),
+    class = c("list", "pv_request")
+  ) -> gp
+
+  if (gp$method == "POST") gp$body <- rawToChar(resp$req$options$postfields)
+  gp
+}
+
+get_data <- function(prsd_resp) {
+  structure(
+    list(prsd_resp[[1]]),
+    names = names(prsd_resp[1]),
+    class = c("list", "pv_data")
+  )
+}
+
+get_query_results <- function(prsd_resp) {
+  structure(
+    prsd_resp[grepl("_count", names(prsd_resp))],
+    class = c("list", "pv_query_results")
+  )
+}
+
+process_resp <- function(resp) {
+  parse_resp(resp = resp) -> prsd_resp
+
+  get_request(resp = resp) -> request
+  get_data(prsd_resp = prsd_resp) -> data
+  get_query_results(prsd_resp = prsd_resp) -> query_results
+
+  structure(
+    list(
+      data_results = data,
+      query_results = query_results,
+      request = request
+    ),
+    class = c("list", "pv_result")
+  )
+}
--- R/search-pv.R
+++ R/search-pv.R
@@ -44,16 +44,72 @@ get_post_body <- function(query, arg_list) {
   gsub('(,"[fs]":)([,}])', paste0('\\1', "{}", '\\2'), body)
 }
 
+warn_for_get <- function(query)
+  if (nchar(query) > 2000) paste0_msg("Your query is over 2,000 characters long, ",
+                                      "I suggest using the POST method instead of GET.")
+
+one_request <- function(method, query, base_url, arg_list, ...) {
+  httr::user_agent("https://github.com/crew102/patentsview") -> ua
+  if (method == "GET") {
+    get_get_url(query = query, base_url = base_url, arg_list = arg_list) -> get_url
+    httr::GET(url = get_url, ua, ...) -> resp
+  } else {
+    get_post_body(query = query, arg_list = arg_list) -> body
+    httr::POST(url = base_url, body = body, ua, ...) -> resp
+  }
+
+  if (httr::http_error(resp)) throw_er(resp = resp)
+
+  process_resp(resp = resp)
+}
+
+request_apply <- function(ex_res, method, query, base_url, arg_list, ...) {
+  ceiling(ex_res$query_results[[1]] / 10000) -> req_pages
+  if (req_pages < 1)
+    stop("No records matched your query...Can't download multiple pages",
+         .call = FALSE)
+  sapply(1:req_pages, FUN = function(i) {
+    arg_list$opts$per_page <- 10000
+    arg_list$opts$page <- i
+    one_request(method = method, query = query, base_url = base_url,
+                arg_list = arg_list, ...) -> x
+    x$data_results
+  }) -> tmp
+  do.call("rbind", tmp)
+}
+
+#' Search patentsview data
+#'
+#' @param query Either a character/json vector of length 1 with valid JSON query syntax or a list to be converted to JSON. See examples and/or query vignette.
+#' @param fields Character vector of data fields that you want returned from the server. A value of \code{NULL} indicates that the default fields should be returned, as per the patentsview API. See \url{http://www.patentsview.org/api/patent.html#field_list} for a list of acceptable fields from the "patents" endpoint. Field lists for the six other endpoints are available on the PatentsView website as well.
+#' @param endpoint Character vector of length 1, consisting of one of the following values: \code{"patents", "inventors", "assignees", "locations", "cpc_subsections", "uspc_mainclasses", "nber_subcategories"}
+#' @param subent_cnts Do you want the total counts of unique subentities to be returned to you? This is equivlient to the \code{include_subentity_total_counts} parameter found here: \url{http://www.patentsview.org/api/query-language.html#options_parameter}. See details for an explanation of subentities.
+#' @param mtchd_subent_only Do you want all the subentities returned to you, even if they would be filtered by your query? This is the \code{matched_subentities_only} parameter found here: \url{http://www.patentsview.org/api/query-language.html#options_parameter}. See details for an explanation of subentities.
+#' @param page The page number of the results that you want returned.
+#' @param per_page The number of records that should be returned per page. This value can be as high as 10,000 (e.g., \code{per_page = 10000}).
+#' @param all_pages Do you want to download all possible pages of output? If \code{all_pages = TRUE}, the values of \code{page} and \code{per_page} don't matter.
+#' @param sort A named character vector where the names indicate the field that is to be sorted by and the values indicate the direction of sorting ("asc" or "desc"). For example, \code{sort = c("patent_number" = "asc")} would be an acceptable value, as would \code{sort = c("patent_number" = "asc", "patent_date" = "desc")}, or \code{sort = NULL} (the default - no sorting is done).
+#' @param method A character vector of length 1 indicating the HTTP method that you want to use to send the request. Options include \code{method = "GET"} or \code{method = "POST"}.
+#' @param ... Arguments passed along to \code{\link[httr]{GET}} or \code{\link[httr]{POST}}.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 search_pv <- function(query,
-                      fields = NULL, # "patent_number",
+                      fields = NULL,
                       endpoint = "patents",
                       subent_cnts = FALSE,
                       mtchd_subent_only = FALSE,
                       page = 1,
                       per_page = 25,
-                      sort = NULL, # c("patent_number" = "asc")
+                      all_pages = FALSE,
+                      sort = NULL,
                       method = "GET",
                       ...) {
+
+  if (is.list(query) && !is.data.frame(query))
+    jsonlite::toJSON(query, auto_unbox = TRUE) -> query
 
   validate_args(query = query, fields = fields, endpoint = endpoint,
                 method = method, subent_cnts = subent_cnts,
@@ -66,16 +122,14 @@ search_pv <- function(query,
 
   get_base(endpoint = endpoint) -> base_url
 
-  if (method == "GET") {
-    get_get_url(query = query, base_url = base_url, arg_list = arg_list) -> url_to_get
-    httr::GET(url = url_to_get, ...) -> out
-  } else {
-    get_post_body(query = query, arg_list = arg_list) -> body_to_post
-    gsub('(,"[fs]":)([,}])', paste0('\\1', "{}", '\\2'),
-         body_to_post) -> body_trans
-  }
+  one_request(method = method, query = query, base_url = base_url,
+              arg_list = arg_list, ...) -> res
 
-  httr::content(out, "text") -> j
-  jsonlite::fromJSON(j) -> j
-  j
+  if (!all_pages) return(res)
+
+  request_apply(ex_res = res, method = method, query = query,
+                base_url = base_url, arg_list = arg_list, ...) -> full_data
+  res$data_results[[1]] <- full_data
+
+  res
 }
