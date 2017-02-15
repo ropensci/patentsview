3c7395820aa8e35bea9053db2ec56b7d2e3a5ef8
Chris Baker <chriscrewbaker@gmail.com>
2017-02-15 13:53:25 -0500

    Pass browser option directly to search_pv

--- R/process-error.R
+++ R/process-error.R
@@ -1,18 +1,3 @@
-have_viewer <- function() {
-  tryCatch(
-    rstudioapi::hasFun("viewer"),
-    error = function(m) FALSE
-  )
-}
-
-view_er_html <- function(er_html) {
-  tempDir <- tempfile()
-  dir.create(tempDir)
-  fi <- file.path(tempDir, "pv_error.html")
-  writeLines(er_html, fi)
-  rstudioapi::viewer(fi)
-}
-
 parse_er_msg <- function(er_html) {
   if (is.na(er_html)) return("")
   gsub(".*<strong>Message:</strong>(.*)</.*File:", "\\1", er_html) -> er_prsd
@@ -20,25 +5,31 @@ parse_er_msg <- function(er_html) {
   ifelse(is.na(er_maybe), "", er_maybe)
 }
 
-custom_er <- function(resp) {
+custom_er <- function(resp, browser) {
   httr::content(resp, as = "text", encoding = "UTF-8") -> er_html
   parse_er_msg(er_html = er_html) -> er_prsd
 
   paste0("Your query returned the following error:", er_prsd) -> gen_er
 
-  getOption("pv_error_viewer") -> view_op_ys
-  have_viewer() -> hv_viewer
+  if (nchar(er_prsd) < 5) {
+    httr::stop_for_status(resp)
+  } else {
+    tempDir <- tempfile()
+    dir.create(tempDir)
+    fi <- file.path(tempDir, "pv_error.html")
+    writeLines(er_html, fi)
+    browseURL(url = fi, browser = browser)
+  }
 
-  if (nchar(er_prsd) < 5) httr::stop_for_status(resp)
-  if (view_op_ys && hv_viewer) view_er_html(er_html = er_html)
-  if (view_op_ys && !hv_viewer)
-    paste0_stop(gen_er, "\n\n", "Note: You need to install the rstudioapi ",
-                "package to view patentsview api errors in your viewer pane.")
   paste0_stop(gen_er)
 }
 
-throw_er <- function(resp) {
+throw_er <- function(resp, browser) {
   httr::http_type(resp) -> typ
   grepl("text|html|xml", typ, ignore.case = TRUE) -> is_txt_html
-  ifelse(is_txt_html, custom_er(resp), httr::stop_for_status(resp))
+  ifelse(
+    is_txt_html,
+    custom_er(resp = resp, browser = browser),
+    httr::stop_for_status(resp)
+  )
 }
--- R/search-pv.R
+++ R/search-pv.R
@@ -48,7 +48,7 @@ warn_for_get <- function(query)
   if (nchar(query) > 2000) paste0_msg("Your query is over 2,000 characters long, ",
                                       "I suggest using the POST method instead of GET.")
 
-one_request <- function(method, query, base_url, arg_list, ...) {
+one_request <- function(method, query, base_url, arg_list, browser, ...) {
   httr::user_agent("https://github.com/crew102/patentsview") -> ua
   if (method == "GET") {
     get_get_url(query = query, base_url = base_url, arg_list = arg_list) -> get_url
@@ -58,12 +58,12 @@ one_request <- function(method, query, base_url, arg_list, ...) {
     httr::POST(url = base_url, body = body, ua, ...) -> resp
   }
 
-  if (httr::http_error(resp)) throw_er(resp = resp)
+  if (httr::http_error(resp)) throw_er(resp = resp, browser = browser)
 
   process_resp(resp = resp)
 }
 
-request_apply <- function(ex_res, method, query, base_url, arg_list, ...) {
+request_apply <- function(ex_res, method, query, base_url, arg_list, browser, ...) {
   ceiling(ex_res$query_results[[1]] / 10000) -> req_pages
   if (req_pages < 1)
     stop("No records matched your query...Can't download multiple pages",
@@ -72,24 +72,25 @@ request_apply <- function(ex_res, method, query, base_url, arg_list, ...) {
     arg_list$opts$per_page <- 10000
     arg_list$opts$page <- i
     one_request(method = method, query = query, base_url = base_url,
-                arg_list = arg_list, ...) -> x
+                arg_list = arg_list, browser = browser, ...) -> x
     x$data_results
   }) -> tmp
-  do.call("rbind", tmp)
+  do.call("rbind", c(tmp, make.row.names = FALSE))
 }
 
-#' Search patentsview data
+#' Search PatentsView data
 #'
-#' @param query Either a character/json vector of length 1 with valid JSON query syntax or a list to be converted to JSON. See examples and/or query vignette.
-#' @param fields Character vector of data fields that you want returned from the server. A value of \code{NULL} indicates that the default fields should be returned, as per the patentsview API. See \url{http://www.patentsview.org/api/patent.html#field_list} for a list of acceptable fields from the "patents" endpoint. Field lists for the six other endpoints are available on the PatentsView website as well.
+#' @param query Either a character or JSON vector of length 1 with valid JSON query syntax or a list to be converted to JSON. See examples and/or query vignette.
+#' @param fields Character vector of data fields that you want returned from the server. A value of \code{NULL} indicates that the default fields should be returned, as per the PatentsView API. See \url{http://www.patentsview.org/api/patent.html#field_list} for a list of acceptable fields from the "patents" endpoint. Field lists for the six other endpoints are available on the PatentsView website as well.
 #' @param endpoint Character vector of length 1, consisting of one of the following values: \code{"patents", "inventors", "assignees", "locations", "cpc_subsections", "uspc_mainclasses", "nber_subcategories"}
 #' @param subent_cnts Do you want the total counts of unique subentities to be returned to you? This is equivlient to the \code{include_subentity_total_counts} parameter found here: \url{http://www.patentsview.org/api/query-language.html#options_parameter}. See details for an explanation of subentities.
 #' @param mtchd_subent_only Do you want all the subentities returned to you, even if they would be filtered by your query? This is the \code{matched_subentities_only} parameter found here: \url{http://www.patentsview.org/api/query-language.html#options_parameter}. See details for an explanation of subentities.
 #' @param page The page number of the results that you want returned.
 #' @param per_page The number of records that should be returned per page. This value can be as high as 10,000 (e.g., \code{per_page = 10000}).
-#' @param all_pages Do you want to download all possible pages of output? If \code{all_pages = TRUE}, the values of \code{page} and \code{per_page} don't matter.
-#' @param sort A named character vector where the names indicate the field that is to be sorted by and the values indicate the direction of sorting ("asc" or "desc"). For example, \code{sort = c("patent_number" = "asc")} would be an acceptable value, as would \code{sort = c("patent_number" = "asc", "patent_date" = "desc")}, or \code{sort = NULL} (the default - no sorting is done).
+#' @param all_pages Do you want to download all possible pages of output? If \code{all_pages = TRUE}, the values of \code{page} and \code{per_page} are ignored.
+#' @param sort A named character vector where the name indicatea the field that is to be sorted by and the value indicates the direction of sorting ("asc" or "desc"). For example, \code{sort = c("patent_number" = "asc")} would be an acceptable value, as would \code{sort = c("patent_number" = "asc", "patent_date" = "desc")}. \code{sort = NULL} (the default) means do not sort the results.
 #' @param method A character vector of length 1 indicating the HTTP method that you want to use to send the request. Options include \code{method = "GET"} or \code{method = "POST"}.
+#' @param browser The program to be used as the HTML browser to view error messages sent by the API. This should be either a character vector of length 1 giving the name of the program (assuming it is in your PATH...e.g., \code{browser = "chrome"}) or the full path to the program (e.g., \code{browser = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"}). Alternatively, you can provide an R function to be called to invoke the browser (e.g., \code{browser = rstudioapi::viewer}). Under Windows \code{NULL} is also allowed (and is the default), and implies that the file association mechanism will be used. To turn the browser off when using Windows, set \code{browser = "false"}.
 #' @param ... Arguments passed along to \code{\link[httr]{GET}} or \code{\link[httr]{POST}}.
 #'
 #' @return
@@ -106,6 +107,7 @@ search_pv <- function(query,
                       all_pages = FALSE,
                       sort = NULL,
                       method = "GET",
+                      browser = getOption("browser"),
                       ...) {
 
   if (is.list(query) && !is.data.frame(query))
@@ -123,7 +125,7 @@ search_pv <- function(query,
   get_base(endpoint = endpoint) -> base_url
 
   one_request(method = method, query = query, base_url = base_url,
-              arg_list = arg_list, ...) -> res
+              arg_list = arg_list, browser = browser, ...) -> res
 
   if (!all_pages) return(res)
 
