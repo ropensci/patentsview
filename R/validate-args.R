586c56b35dfeb3487744a26814cde22a71bdf933
Chris Baker <chriscrewbaker@gmail.com>
2017-02-16 20:42:48 -0500

    Clean up arg. validation function

--- R/validate-args.R
+++ R/validate-args.R
@@ -9,18 +9,17 @@ validate_args <- function(query, fields, endpoint, method, subent_cnts,
        "endpoint must be a length 1 vector and be one of: ",
               paste(ok_ends, collapse = ", "))
   asrt(all(method %in% ok_meth, length(method) == 1),
-       "method must be a length 1 vector and be one of: ",
-              paste(ok_meth, collapse = ", "))
+       "method must be either 'GET' or 'POST'")
 
   asrt(all(is.logical(subent_cnts), length(subent_cnts) == 1),
        "subent_cnts must be either TRUE or FALSE")
   asrt(all(is.logical(mtchd_subent_only), length(mtchd_subent_only) == 1),
        "mtchd_subent_only must be either TRUE or FALSE")
 
-  asrt(all(is.numeric(page), length(page) == 1, page >= 1, page <= 10000),
-       "page must be a length 1 numeric vector between 1 and 10,000")
-  asrt(all(is.numeric(per_page), length(per_page) == 1),
-       "per_page must be a length 1 numeric vector")
+  asrt(all(is.numeric(page), length(page) == 1, page >= 1),
+       "page must be a length 1 numeric vector greater than 1")
+  asrt(all(is.numeric(per_page), length(per_page) == 1, per_page <= 10000),
+       "per_page must be a length 1 numeric vector less than or equal to 10,000")
 
   if (!is.null(sort))
     asrt(all(all(names(sort) %in% fields), all(sort %in% c("asc", "desc")), !is.list(sort)),
@@ -28,4 +27,10 @@ validate_args <- function(query, fields, endpoint, method, subent_cnts,
          "specified in the field argument. See examples")
 
   asrt(jsonlite::validate(query), "query is not valid json.")
+
+  patentsview:::fields -> flds
+  flds[flds$endpoint == endpoint, "field"] -> flds_flt
+  asrt(all(fields %in% flds_flt),
+       "Bad field(s): ",
+       paste(fields[!(fields %in% flds_flt)], collapse = ", "))
 }
