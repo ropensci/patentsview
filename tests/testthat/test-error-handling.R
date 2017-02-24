b961db819d051e5fc96edb4a249c234a09548c36
Chris Baker <chriscrewbaker@gmail.com>
2017-02-23 23:32:07 -0500

    Fill out tests a bit more

--- tests/testthat/test-error-handling.R
+++ tests/testthat/test-error-handling.R
@@ -2,16 +2,16 @@ context("http_error_handling")
 
 test_that("Bad query arguments get informative error messages", {
 
-  tojson_2(list("patent_datee" = "1976-01-01")) -> bad_qr
+  patentsview:::tojson_2(list("patent_datee" = "1976-01-01")) -> bad_qr
   expect_error(search_pv(query = bad_qr, error_browser = "false"), "patent_datee")
 
-  tojson_2(list(
+  patentsview:::tojson_2(list(
     "_neq" = list("patent_date" = c("1976-01-01", "1976-01-01"))
   )) -> bad_qr
   expect_error(search_pv(query = bad_qr, error_browser = "false"), "array")
 
 
-  tojson_2(list(
+  patentsview:::tojson_2(list(
     "_contains" = list("assignee_state" = c("new"))
   )) -> bad_qr
   search_pv(query = bad_qr, error_browser = "false")
--- tests/testthat/test-search-pv.R
+++ tests/testthat/test-search-pv.R
@@ -13,3 +13,36 @@ test_that("API returns expected df names", {
   expect_equal(eps, z)
 
 })
+
+test_that("A larger set of fields can be returned", {
+
+  c("assignees", "cpc_subsections", "inventors", "locations",
+    "nber_subcategories", "patents", "uspc_mainclasses") -> eps
+
+  sapply(eps,
+         FUN = function(x) {
+           get_fields(x, "patents") -> y
+           search_pv('{"patent_number":"5116621"}', endpoint = x, fields = y)
+         }) -> z
+
+  expect_true(TRUE)
+
+})
+
+test_that("DSL language-based query returns expected results", {
+
+  with_qfuns(
+    and(
+      or(
+        gte(patent_date = '2014-01-01'),
+        lte(patent_date = '1978-01-01')
+      ),
+      text_phrase(patent_abstract = c("computer program", "dog leash"))
+    )
+  ) -> query
+
+  search_pv(query = query, endpoint = "patents") -> out
+
+  expect_gt(out$query_results$total_patent_count, 1000)
+
+})
