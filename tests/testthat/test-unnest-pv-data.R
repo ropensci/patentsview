b9a20a1080c68106fad64b1588aef0b0c540a01c
Chris Baker <chriscrewbaker@gmail.com>
2017-05-30 15:57:27 -0400

    Add test for unnest_pv_data

---
+++ tests/testthat/test-unnest-pv-data.R
@@ -0,0 +1,17 @@
+context("unnest_pv_data")
+
+eps <- get_endpoints()
+
+test_that("", {
+  skip_on_cran()
+
+  eps_no_loc <- eps[eps != "locations"]
+
+  z <- lapply(eps_no_loc, FUN = function(x) {
+    pv_out <- search_pv("{\"patent_number\":\"5116621\"}", endpoint = x,
+                        fields = get_fields(x))
+    unnest_pv_data(pv_out[["data"]])
+  })
+
+  expect_true(TRUE)
+})
