8b1c9084b9106eb0a5448d2467c4cdc1ffed7fc9
Chris Baker <chriscrewbaker@gmail.com>
2017-06-01 13:16:13 -0400

    Check in inst

---
+++ inst/doc/patentsview.R
@@ -0,0 +1,106 @@
+params <-
+structure(list(eval_all = TRUE), .Names = "eval_all")
+
+## ---- echo = FALSE, message = FALSE--------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval = FALSE--------------------------------------------------------
+#  devtools::install_github("crew102/patentsview")
+
+## ------------------------------------------------------------------------
+library(patentsview)
+
+search_pv(query = '{"_gte":{"patent_date":"2007-01-01"}}',
+          fields = c("patent_number", "patent_title"), 
+          endpoint = "patents")
+
+## ------------------------------------------------------------------------
+qry_funs$gte(patent_date = "2007-01-01")
+
+## ------------------------------------------------------------------------
+with_qfuns(
+  and(
+    gte(patent_date = "2007-01-01"),
+    text_phrase(patent_abstract = c("computer program", "dog leash"))
+  )
+)
+
+## ----eval = params$eval_all----------------------------------------------
+search_pv(query = qry_funs$eq(inventor_last_name = "chambers"),
+          page = 2, per_page = 150) # gets records 150 - 300
+
+## ----eval = params$eval_all----------------------------------------------
+search_pv(query = qry_funs$eq(inventor_last_name = "chambers"),
+          all_pages = TRUE)
+
+## ------------------------------------------------------------------------
+search_pv(query = qry_funs$eq(inventor_last_name = "chambers"), 
+          endpoint = "patents", 
+          fields = c("patent_number", "inventor_last_name", 
+                     "assignee_organization"))
+
+## ------------------------------------------------------------------------
+search_pv(query = qry_funs$eq(inventor_last_name = "chambers"), 
+          endpoint = "assignees", 
+          fields = c("patent_number", "inventor_last_name", 
+                     "assignee_organization"))
+
+## ----eval = params$eval_all----------------------------------------------
+search_pv(query = qry_funs$gt(patent_num_cited_by_us_patents = 500))
+
+## ----eval = params$eval_all----------------------------------------------
+# Setting subent_cnts = TRUE will give us the subentity counts.  
+# In this case, inventors are subentities so we will get their counts.
+search_pv(query = qry_funs$gt(patent_num_cited_by_us_patents = 500),
+          fields = c("patent_number", "inventor_id"), subent_cnts = TRUE)
+
+## ----eval = params$eval_all----------------------------------------------
+# Get all possible assignee-level and patent-level data fields available for the assignees endpoint:
+asgn_pat_flds <- get_fields("assignees", c("assignees", "patents"))
+
+# Ask the PatentsView API for these fields:
+search_pv(query = qry_funs$contains(inventor_last_name = "smith"), 
+          endpoint = "assignees", fields = asgn_pat_flds)
+
+## ----eval = params$eval_all----------------------------------------------
+search_pv(query = qry_funs$contains(govint_org_name = 'department of energy'), 
+          endpoint = "cpc_subsections", 
+          fields =  "cpc_total_num_patents",
+          sort = c("cpc_total_num_patents" = "desc"), 
+          per_page = 10)
+
+## ------------------------------------------------------------------------
+query <- with_qfuns(
+  text_any(patent_abstract = 'tool animal')
+)
+
+## ------------------------------------------------------------------------
+query_1a <- with_qfuns(
+  and(
+    text_any(patent_abstract = 'tool animal'),
+    lte(patent_date = "2010-01-01")
+  )
+)
+
+query_1b <- with_qfuns(
+  and(
+    text_any(patent_abstract = 'tool animal'),
+    gt(patent_date = "2010-01-01")
+  )
+)
+
+## ------------------------------------------------------------------------
+res <- search_pv(query = qry_funs$contains(inventor_last_name = "smith"), 
+                 endpoint = "assignees", 
+                 fields = get_fields("assignees", c("assignees","applications", 
+                                                    "gov_interests")))
+res$data
+
+## ------------------------------------------------------------------------
+new_data <- unnest_pv_data(data = res$data, pk = "assignee_id")
+
+new_data
+
