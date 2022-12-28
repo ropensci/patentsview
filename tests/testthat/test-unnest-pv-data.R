bf3712562abd203c52e28eb42f8577def1d79c29
Russ Allen <rrjallen@yahoo.com>
2022-12-28 09:32:13 -0600

    API bug work arounds and tests for fixes

--- tests/testthat/test-unnest-pv-data.R
+++ tests/testthat/test-unnest-pv-data.R
@@ -4,20 +4,39 @@ eps <- get_endpoints()
 
 test_that("", {
   skip_on_cran()
-  # TODO(any): Remove:
-  skip("Temp skip for API redesign PR")
 
-  eps_no_loc <- eps[eps != "locations"]
+  # TODO(any): add back fields = get_fields(x)
+  # API throws 500s if some nested fields are included
 
-  z <- lapply(eps_no_loc, function(x) {
+  # locations endpoint is back but it fails this test
+  # Error: Invalid field: location_num_assignees
+  # Error: Invalid field: attorney_first_seen_date
+  bad_eps <- c("locations", "patent/attorneys")
+
+  good_eps <- eps[!eps %in% bad_eps]
+
+  z <- lapply(good_eps, function(x) {
     Sys.sleep(1)
+    print(x)
     pv_out <- search_pv(
-      "{\"patent_number\":\"5116621\"}",
+      query = TEST_QUERIES[[x]],
       endpoint = x,
-      fields = get_fields(x)
+      fields = get_fields(x, group = (x)) # requesting non-nested attributes
     )
     unnest_pv_data(pv_out[["data"]])
   })
 
   expect_true(TRUE)
+
+  # This test will fail when the API is fixed
+  z <- lapply(bad_eps, function(x) {
+    Sys.sleep(1)
+    expect_error(
+      pv_out <- search_pv(
+        query = TEST_QUERIES[[x]],
+        endpoint = x,
+        fields = get_fields(x, group = (x)) # requesting non-nested attributes
+      )
+    )
+  })
 })
