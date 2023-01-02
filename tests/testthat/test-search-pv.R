64d01269d8b2fce33ec6c931359c1ec87fdb5838
Russ Allen <rrjallen@yahoo.com>
2023-01-02 08:39:15 -0600

    throttling test update

--- tests/testthat/test-search-pv.R
+++ tests/testthat/test-search-pv.R
@@ -96,6 +96,8 @@ test_that("search_pv properly URL encodes queries", {
 
 })
 
+context("throttle")
+
 # Below we request the same data in built_singly and result_all, with the only
 # difference being that we intentionally get throttled in built_singly by
 # sending one request per patent number (instead of all requests at once). If
@@ -106,14 +108,18 @@ test_that("Throttled requests are automatically retried", {
   res <- search_pv('{"_gte":{"patent_date":"2007-01-04"}}', per_page = 50)
   patent_ids <- res$data$patents$patent_id
 
-  built_singly <- lapply(patent_ids, function(patent_id) {
-    search_pv(
-      query = qry_funs$eq(patent_id = patent_id),
-      endpoint = "patent/us_patent_citations",
-      fields = c("patent_id", "citation_patent_id"),
-      sort = c("citation_patent_id" = "asc")
-    )[["data"]][["us_patent_citations"]]
-  })
+  expect_message(
+    built_singly <- lapply(patent_ids, function(patent_id) {
+      search_pv(
+        query = qry_funs$eq(patent_id = patent_id),
+        endpoint = "patent/us_patent_citations",
+        fields = c("patent_id", "citation_patent_id"),
+        sort = c("citation_patent_id" = "asc")
+      )[["data"]][["us_patent_citations"]]
+    }),
+    "The API's requests per minute limit has been reached. "
+  )
+
   built_singly <- do.call(rbind, built_singly)
 
   result_all <- search_pv(
