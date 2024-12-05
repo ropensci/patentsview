5d8502541ea1b31958d5291065ddcb8222f5e3a1
Russ Allen <rrjallen@yahoo.com>
2024-12-05 14:09:19 -0600

    endpoint name changes

--- R/search-pv.R
+++ R/search-pv.R
@@ -201,7 +201,7 @@ request_apply <- function(ex_res, method, query, base_url, arg_list, api_key, ..
 #'
 #' search_pv(
 #'   query = qry_funs$gt(patent_year = 2010),
-#'   fields = get_fields("patents", c("patents", "assignees_at_grant"))
+#'   fields = get_fields("patent", c("patents", "assignees"))
 #' )
 #'
 #' search_pv(
@@ -212,14 +212,14 @@ request_apply <- function(ex_res, method, query, base_url, arg_list, api_key, ..
 #' )
 #'
 #' search_pv(
-#'   query = qry_funs$eq(name_last = "crew"),
-#'   endpoint = "inventors",
+#'   query = qry_funs$eq(inventor_name_last = "Crew"),
+#'   endpoint = "inventor",
 #'   all_pages = TRUE
 #' )
 #'
 #' search_pv(
-#'   query = qry_funs$contains(name_last = "smith"),
-#'   endpoint = "assignees"
+#'   query = qry_funs$contains(assignee_individual_name_last = "Smith"),
+#'   endpoint = "assignee"
 #' )
 #'
 #' search_pv(
