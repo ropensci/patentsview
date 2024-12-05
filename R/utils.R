2e9435e015dbcb435ded6d7b374ed22606a5985a
Russ Allen <rrjallen@yahoo.com>
2024-12-05 12:58:32 -0600

    put back to_plural()

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,22 @@ format_num <- function(x) {
     big.mark = ",", scientific = FALSE, trim = TRUE
   )
 }
+
+#' @noRd
+to_plural <- function(singular) {
+  # wipo endpoint returns singular wipo as the entity
+
+  # remove the patent/ and publication/ from nested endpoints when present
+  singular <- sub("^(patent|publication)/", "", singular)
+
+  if (singular == "ipc") {
+    plural <- "ipcr"
+  } else if (singular == "wipo") {
+    plural <- singular
+  } else if (endsWith(singular, "s")) {
+    plural <- paste0(singular, "es")
+  } else {
+    plural <- paste0(singular, "s")
+  }
+  plural
+}
